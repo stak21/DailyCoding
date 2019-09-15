d686a2b7b6455b1d583b8f8cf8a4d2c722773e35
stak21 <stakashima21@gmail.com>
2019-09-15 14:16:47 -0700

    LeetCode(update): Improved On^2 to O(n)

--- leetCodeChallenge/1_two_sum.py
+++ leetCodeChallenge/1_two_sum.py
@@ -7,19 +7,40 @@ def two_sum(nums, target):
 
 print(two_sum([1,2,3, 10], 12))
 """
-
-""" Hash pass """
 def two_sum(nums, target):
+    """ Hash two pass """
+    """
+        h = {}
+        for count, i in enumerate(nums):
+            h[i] = count
+
+        for count, j in enumerate(nums):
+            dif = target - j
+            found = h.get(dif)
+            
+            if found and found != count:
+                return [count, found]
+    """
+    """ Hash one pass attempt 1 """
+    """
+        h = {}
+        for count, i in enumerate(nums):
+            dif = target - i
+            found = h.get(dif)
+            h[i] = count
+            if (found or found == 0) and found != count:
+                return [found, count]
+    """
+
+    """ attempt 2 """
     h = {}
     for count, i in enumerate(nums):
-        h[i] = count
+        dif = target - i
+        if dif in h:
+            return [h[dif], count]
+        else:
+            h[i] = count
 
-    for count, j in enumerate(nums):
-        dif = target - j
-        found = h.get(dif)
-        
-        if found and found != count:
-            return [count, found]
 
 print(two_sum([1,2,3, 10], 12))
 print(two_sum([3,1,13, 10], 16))
