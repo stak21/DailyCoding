4805f334723815685b34c8ff3f077ded09673ac6
stak21 <stakashima21@gmail.com>
2019-09-22 20:35:27 -0700

    LeetCode(Success): Reverses a linkedlist

---
+++ leetCodeChallenge/206_reversed_ll.py
@@ -0,0 +1,9 @@
+   def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        cur = head
+        while cur:
+            temp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = temp
+        return prev
