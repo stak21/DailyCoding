daba7fb5c54a218c0b8f7fb5131948e077416722
stak21 <stakashima21@gmail.com>
2019-09-24 09:39:21 -0700

    MockCode(Success): Flatten an array of objects into the given file structure

---
+++ mock_interview/flatten_to_dict.py
@@ -0,0 +1,78 @@
+def solution(arr):
+    #  def dfs(arr, target):
+        #  if not arr:
+            #  return
+        #  # iterate through the children of arr as file
+        #  for file in arr:
+            #  if file['id'] == 3:
+                #  print(file)
+            #  if target is file['id']:
+                #  return file
+            #  file = dfs(file['children'], target)
+            #  if file:
+                #  return file
+    def dfs(arr, path):
+        if not arr:
+            return
+        # iterate through the children of arr as file
+        for file in arr:
+            if path[0] is file['id']:
+                if len(path) == 1:
+                    return file
+                subfolder = dfs(file['children'], path[1:])
+                if subfolder:
+                    return subfolder 
+        
+    if not arr:
+        return []
+    # sorted(arr, key=lambda node: node['parent'])
+    folder = []
+    path = []
+    memory = {}
+    for node in arr:
+        node['children'] = []
+        parent = node['parent']
+        memory[node['id']] = parent
+        if parent == 0:
+            folder.append(node)
+        else:
+            path = [parent]
+            # Creates a path to look for the target subfolder
+            while True:
+                memory_parent = memory.get(path[0])
+                if memory_parent is not None and memory_parent == 0:
+                    break
+                path.insert(0, memory_parent)
+            sub = dfs(folder, path)
+            sub['children'].append(node)
+
+
+            #  parent = node['parent']
+            #  file = dfs(folder, parent)
+            #  if file:
+                #  file['children'].append(node)
+
+    return folder
+    
+    
+    
+arr = [
+    {'id':1 ,'parent' : 0},
+    {'id':5 ,'parent' : 0},
+    {'id':6 ,'parent' : 0},
+    {'id':3 ,'parent' : 1},
+    {'id':2 ,'parent' : 1},
+    {'id':8 ,'parent' : 1},
+    {'id':4 ,'parent' : 2},
+    {'id':7 ,'parent' : 4}
+]
+
+result = solution(arr)
+
+
+import json
+def pprint(data):
+    print(json.dumps(data, indent=4, sort_keys=True))
+    
+    
+pprint(result)
