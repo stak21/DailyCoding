8c2c93573b45061ae5f9b151223a73c55090c69d
Shoji <stakashima21@gmail.com>
2019-12-02 22:51:57 -0800

    codewars(prep): 10:51 pm

--- codewarsCodeChallenge/4-secret_triplet.py
+++ codewarsCodeChallenge/4-secret_triplet.py
@@ -1,6 +1,28 @@
 # Requirements:
+#   The secret string to return is the first sequence 
+#   Need to iterate through the entire string because the last character could make the shortest secret string
 # Input:
-# Output:
+#   ex: 'hello' ['hll', 'hel'] would return 'hel' because hel occurs before hll
+# Process:
+#   thoughts -  I could add up the index of every character's index
+#       ex - h = 0,  l = 2, l = 3 -> 6
+#               h = 0, e =1 , l = 2 -> 5
+#               return the least 
+#       another idea would be to take the first character and create a list of all the ones with the first character
+#       then the next until there is one
+#       if there is none for that character then reuse the previous list instead of the empty list
+#       if there is more than 1 in the list, use that list
+#       if there is just 1 return that string
+#   Approach one:
+#   iterate through the string
+#       for each character, iterate through the triplets
+#           using the triplet_index, return a list of triplets that have the matching character in the triplet_index
+#           if the list is empty, move on
+#           if the list length is 1, return the triplet
+#           else  make that the new list 
 
-def secret_triplet:
+
+def secret_triplet(secret, triplets):
     # Return the secret string
+    # approach one:
+    
