29bac56f8923fec2f32b84a47d1e7d9aa4a53b36
Shoji <stakashima21@gmail.com>
2019-12-09 00:02:34 -0800

    codewars(tests): 12/9 12:02 personal tests passed with these changes

--- codewarsCodeChallenge/5-page_helper.py
+++ codewarsCodeChallenge/5-page_helper.py
@@ -1,3 +1,5 @@
+import math
+
 # Requirements: 
 #   Create a utility class for pagination to access information about the pages.
 #   The class is designed to take in an array of values and an integer indicating how many items will be allowed per each page. 
@@ -9,13 +11,11 @@
 # print(answer[0] == helper.page_item_count(0)  # should == 4
 # print(answer[0] == helper.page_item_count(1) # last page - should == 2
 # print(answer[0] == helper.page_item_count(2) # should == -1 since the page is invalid
-print(answer[0] == 
 # # page_ndex takes an item index and returns the page that it belongs on
 # helper.page_index(5) # should == 1 (zero based index)
 # print(answer[0] == helper.page_index(2) # should == 0
 # print(answer[0] == helper.page_index(20) # should == -1
 # print(answer[0] == helper.page_index(-10) # should == -1 because negative indexes are invalid
-print(answer[0] == 
 # Process:
 # create a class that has 
 #   attributes: 
@@ -43,18 +43,20 @@ print(answer[0] ==
 #           5 / 4 == 1
 #           8 / 4 == 2
 
-class PaginationHelper:
+class PaginationHelper():
     def __init__(self, arr, page_cap):
         item_count = len(arr)
         self.item_count = item_count
-        self.page_count = item_count // page_cap
+        self.page_count = max(math.ceil(item_count / page_cap), 1)
         self._page_cap = page_cap
     
     def page_item_count(self, page):
-        if page < 0 or  page > self.page_count:
+        if page < 0 or  page > self.page_count - 1:
             return -1
-        if page == page_count:
-            return self.item_count % self._page_cap
+        if page == self.page_count - 1:
+            count =  self.item_count % self._page_cap 
+            if count > 0:
+                return count
         return self._page_cap
     
     def page_index(self, index):
@@ -73,21 +75,23 @@ tests = [
 
 answers = [
     [2, 6, 4, 2, -1, 1, 0, -1, -1],
-    [6, 6, 1, 1, -1, 5, 0, -1, -1],
-    [1, 6, 8, 6, -1, 0, 0, -1, -1],
+    [6, 6, 1, 1, -1, 5, 2, -1, -1],
+    [1, 6, 6, -1, -1, 0, 0, -1, -1],
     [2, 8, 4, 4, -1, 1, 0, -1, -1],
-    [3, 9, 4, 1, -1, 1, 0, -1, -1],
-    [1, 0, 4, 0, -1, -1, -1, -1, -1],
+    [3, 9, 4, 4, -1, 1, 0, -1, -1],
+    [1, 0, 4, -1, -1, -1, -1, -1, -1],
 ]
 
 for test, answer in zip(tests, answers):
     helper = PaginationHelper(test[0], test[1])
-    print(answer[0] == helper.page_count) # should == 2
-    print(answer[1] == helper.item_count) # should == 6
-    print(answer[2] == helper.page_item_count(0))  # should == 4
-    print(answer[3] == helper.page_item_count(1)) # last page - should == 2
-    print(answer[4] == helper.page_item_count(4)) # should == -1 since the page is invalid
-    print(answer[5] == helper.page_index(5)) # should == 1 (zero based index)
-    print(answer[6] == helper.page_index(2)) # should == 0
-    print(answer[7] == helper.page_index(20)) # should == -1
-    print(answer[8] == helper.page_index(-10)) # should == -1 because negative indexes are invalid
+    print('--------------------')
+    print(answer[0] == helper.page_count, helper.page_count, 'expected to be: ', answer[0]) # should == 2
+    print(answer[1] == helper.item_count, helper.item_count, 'expected to be: ', answer[1]) # should == 6
+    print(answer[2] == helper.page_item_count(0), helper.page_item_count(0), 'expected to be: ', answer[2])  # should == 4
+    print(answer[3] == helper.page_item_count(1), helper.page_item_count(1), 'expected to be: ', answer[3]) # last page - should == 2
+    print(answer[4] == helper.page_item_count(14), helper.page_item_count(14), 'expected to be: ', answer[4]) # should == -1 since the page is invalid
+    print(answer[5] == helper.page_index(5), helper.page_index(5), 'expected to be: ', answer[5]) # should == 1 (zero based index)
+    print(answer[6] == helper.page_index(2), helper.page_index(2), 'expected to be: ', answer[6]) # should == 0
+    print(answer[7] == helper.page_index(20), helper.page_index(20), 'expected to be: ', answer[7]) # should == -1
+    print(answer[8] == helper.page_index(-10), helper.page_index(-10), 'expected to be: ', answer[8]) # should == -1 because negative indexes are invalid
+    print('--------------------')
