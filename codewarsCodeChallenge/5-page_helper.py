34a2ac40b845c0b5bbcd892255835dc075703c6d
Shoji <stakashima21@gmail.com>
2019-12-08 23:38:39 -0800

    codewars(tests): 12/8 11:38

--- codewarsCodeChallenge/5-page_helper.py
+++ codewarsCodeChallenge/5-page_helper.py
@@ -4,18 +4,18 @@
 #   The types of values contained within the collection/array are not relevant.
 # I/O:
 # helper = PaginationHelper(['a','b','c','d','e','f'], 4)
-# helper.page_count # should == 2
-# helper.item_count # should == 6
-# helper.page_item_count(0)  # should == 4
-# helper.page_item_count(1) # last page - should == 2
-# helper.page_item_count(2) # should == -1 since the page is invalid
-
+# print(answer[0] == helper.page_count # should == 2
+# print(answer[0] == helper.item_count # should == 6
+# print(answer[0] == helper.page_item_count(0)  # should == 4
+# print(answer[0] == helper.page_item_count(1) # last page - should == 2
+# print(answer[0] == helper.page_item_count(2) # should == -1 since the page is invalid
+print(answer[0] == 
 # # page_ndex takes an item index and returns the page that it belongs on
 # helper.page_index(5) # should == 1 (zero based index)
-# helper.page_index(2) # should == 0
-# helper.page_index(20) # should == -1
-# helper.page_index(-10) # should == -1 because negative indexes are invalid
-
+# print(answer[0] == helper.page_index(2) # should == 0
+# print(answer[0] == helper.page_index(20) # should == -1
+# print(answer[0] == helper.page_index(-10) # should == -1 because negative indexes are invalid
+print(answer[0] == 
 # Process:
 # create a class that has 
 #   attributes: 
@@ -62,3 +62,32 @@ class PaginationHelper:
             return -1
         return index // self._page_cap
 
+tests = [
+    (['a','b','c','d','e','f'], 4),
+    (['a','b','c','d','e','f'], 1),
+    (['a','b','c','d','e','f'], 8),
+    (['a','b','c','d','e','f', 'g', 'h'], 4),
+    (['a','b','c','d','e','f', 'g', 'h', 'i'], 4),
+    ([], 4),
+]
+
+answers = [
+    [2, 6, 4, 2, -1, 1, 0, -1, -1],
+    [6, 6, 1, 1, -1, 5, 0, -1, -1],
+    [1, 6, 8, 6, -1, 0, 0, -1, -1],
+    [2, 8, 4, 4, -1, 1, 0, -1, -1],
+    [3, 9, 4, 1, -1, 1, 0, -1, -1],
+    [1, 0, 4, 0, -1, -1, -1, -1, -1],
+]
+
+for test, answer in zip(tests, answers):
+    helper = PaginationHelper(test[0], test[1])
+    print(answer[0] == helper.page_count) # should == 2
+    print(answer[1] == helper.item_count) # should == 6
+    print(answer[2] == helper.page_item_count(0))  # should == 4
+    print(answer[3] == helper.page_item_count(1)) # last page - should == 2
+    print(answer[4] == helper.page_item_count(4)) # should == -1 since the page is invalid
+    print(answer[5] == helper.page_index(5)) # should == 1 (zero based index)
+    print(answer[6] == helper.page_index(2)) # should == 0
+    print(answer[7] == helper.page_index(20)) # should == -1
+    print(answer[8] == helper.page_index(-10)) # should == -1 because negative indexes are invalid
