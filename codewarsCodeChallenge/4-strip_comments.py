0427cb3bc08b2772dd9f53906ee3e4ff88522b6c
Shoji <stakashima21@gmail.com>
2019-12-09 23:32:25 -0800

    codewars(attempt 1): 12/9 11:32

--- codewarsCodeChallenge/4-strip_comments.py
+++ codewarsCodeChallenge/4-strip_comments.py
@@ -2,8 +2,10 @@
 #   Strip the comments ( which are passed in ) and everything else after
 #   Remove all trailing white spaces
 # I/O:
-#  ('hello, there everyone I am # your father', ['#']) => 'hello, there everyone I am'
-#   ('hello, there # commented out \n to be there \n Is sto # ry time is gone', ['#']) => 'hello, there \n to be there \n is sto'
+#        I =>('hello, there everyone I am # your father', ['#']) 
+#       O => 'hello, there everyone I am'
+#        I => ('hello, there # commented out \n to be there \n Is sto # ry time is gone', ['#']) 
+#       O => 'hello, there \n to be there \n is sto'
 #
 # Process:
 #   Thoughts: A simple approach would be to iterate through the whole string
@@ -11,3 +13,24 @@
 #           continue on until newline or end of string
 #           if a newline is hit, slice everything from there to the newline 
 #           continue from the newline and repeat
+
+def strip_comments(string, comments):
+    start = 0
+    return_string = []
+    stripping = False
+    for index, c in enumerate(string):
+        if c in comments and stripping is False:
+            return_string.append(string[start:index])
+            stripping = True
+            continue
+        if c == '\n' and stripping is True:
+            # splice
+            start = index
+            stripping = False
+
+    if stripping is False:
+        return_string.append(string[start:].strip(' '))
+    return ' '.join(return_string)
+
+test = 'string is # live'
+print(strip_comments(test, ['#']))
