95b1fbc9d9716a84b86da58b5b40e1a4ecf0f546
Shoji <stakashima21@gmail.com>
2019-12-11 23:52:16 -0800

    codewars(attempt 1): 12/11 11:52 PM

--- codewarsCodeChallenge/reviews/r1_are_they_the_same.py
+++ codewarsCodeChallenge/reviews/r1_are_they_the_same.py
@@ -1,4 +1,16 @@
 # Requirements: Return if the two arrays are "same"
 #       the second array should have all the squared numbers on the right
-#   I/O
+#   I/O: [2,4], [4, 16] => True
 #   Process
+#       One approach would be to sort each one and iterate through checking if they are a pair.
+#           if not return False
+
+# approach one
+def same(a1, a2):
+    if len(a1) != len(a2):
+        return False
+    unique = set(a2)
+    for e in a1:
+        if e ** 2 not in unique:
+            return False
+    return True
