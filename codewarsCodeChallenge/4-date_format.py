7d1d32c5bde3ed2251c886771aeb373a0cfe3068
Shoji <stakashima21@gmail.com>
2019-12-16 23:15:41 -0800

    codewars(attempt): 11:15 PM

--- codewarsCodeChallenge/4-date_format.py
+++ codewarsCodeChallenge/4-date_format.py
@@ -1,5 +1,6 @@
 # Start: 12/16/19 10:32 PM
 #   Prep: 10:51 PM
+#   Attempt: 11:15 PM
 # Requirements:
 #   Given a non negative integer
 #   return a string with the date in human readable format
@@ -23,3 +24,27 @@
 #       if time > 1:
 #           append an s 
 #       return string
+
+def format_duration(seconds):
+    times_dict = {second: 1, minute: 60, hour: 3600, day: 3600*24, year: 3600*24*365 }
+    components = [0,0,0,0, 0]
+    times = ['year', 'day', 'hour', 'minute', 'second']
+    formatted_time = ''
+    for index, time in enuemrate(times):
+        components[index] = seconds // times_dict[time] 
+        seconds %= times_dict[time]
+    if components[4] > 0:
+        if components[4] > 1:
+            times[4] += 's'
+        formatted_time += '{} second'.format(components[4], times[4])
+    if components[3] > 0:
+        attatchment = 'and '
+        formatted_time = attatchment + formatted_time
+    for index in range(3, -1, -1):
+        count = components[index]
+        if  count == 0:
+            continue
+        if count > 1:
+            times[index] += 's'
+        formatted_time += '{} {}, '.format(count, times[index])
+            
