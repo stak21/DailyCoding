eee6603342b3bbb471312c3162dbdee32c989d23
Shoji <stakashima21@gmail.com>
2019-12-17 22:59:07 -0800

    codewars(init): 12/17/19 10:56 PM

---
+++ codewarsCodeChallenge/5-double_cola.py
@@ -0,0 +1,6 @@
+# Started: 12/17/19 10:56 PM
+
+#   Requirements:
+#   Given a list of names in a queue, each name that gets popped returns to the queue, but as two. Return the name of the nth person that gets popped
+#   I/O:
+#   Process: 
