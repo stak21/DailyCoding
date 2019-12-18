1aba310fc94321ff63be1e6c7ceda302b87399c9
Shoji <stakashima21@gmail.com>
2019-12-17 23:22:29 -0800

    codewars(personal): success 11:22PM

--- codewarsCodeChallenge/5-double_cola.py
+++ codewarsCodeChallenge/5-double_cola.py
@@ -15,3 +15,27 @@
 #   Ideas - recursion pass in the array with the number
 #                   each pass through, it will pop the first item and add 2 to the end
 #                   once it reaches 0, return the name at the end
+
+def who_is_next(names, r):
+    if not names:
+        return None
+    if r <= 1:
+        return  names[0]
+    name = names.pop(0)
+    names.extend([name] * 2)
+    return who_is_next(names, r - 1)
+
+tests = [
+    (['shoji'], 1),
+    (['shoji'], 2),
+    (['shoji', 'john'], 2),
+    (['shoji', 'john'], 3),
+    (['shoji', 'john', 'sara'], 3),
+    (['shoji', 'jogn', 'sara'], 4),
+    (['shoji', 'john', 'sara'], 10),
+    ]
+answers = ['shoji', 'shoji', 'john', 'shoji', 'sara', 'shoji', 'shoji']
+
+for test, answer in zip(tests, answers):
+    res = who_is_next(test[0], test[1])
+    print(res, 'Success: ', answer == res)
