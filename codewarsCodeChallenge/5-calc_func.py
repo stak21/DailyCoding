f5ef920521b140218b36edc79b1fb04d5b8ff374
Shoji <stakashima21@gmail.com>
2019-12-23 18:01:30 -0800

    codewars(initial): 12/23/19 6:01 PM

---
+++ codewarsCodeChallenge/5-calc_func.py
@@ -0,0 +1,8 @@
+# 12/23/19 6:01 PM
+
+# Requirements: Create a  function for each digit and each mathmatical operation
+#   The operation will work on two numbers
+#   To call an operation, it must be called by a digit function and the operation will take the inner digit function
+#   Return integers
+#  I/O:
+#  Process:
