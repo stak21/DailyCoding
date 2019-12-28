d48aded8bcd329f0f83507a954e4b0b80e97766f
Shoji <stakashima21@gmail.com>
2019-12-28 10:18:54 -0800

    codewars(initial): 12/28 10:18 AM

--- codewarsCodeChallenge/5-calc_func.py
+++ codewarsCodeChallenge/5-calc_func.py
@@ -1,8 +1,15 @@
-# 12/23/19 6:01 PM
+# 12/28/19 10:18 AM
 
 # Requirements: Create a  function for each digit and each mathmatical operation
 #   The operation will work on two numbers
 #   To call an operation, it must be called by a digit function and the operation will take the inner digit function
 #   Return integers
 #  I/O:
+#   five(plus(three())) => 8
+#   five(times(zero())) => 0
+#   five(divided_by(six())) => 0
+#   five(divided_by(two())) => 2
+#   zero(divided_by(five())) => 0
+#   five(divided_by(zero())) => error
 #  Process:
+#   So if the number 
