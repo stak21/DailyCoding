e49fcdd8737b6e178c1f8e1af65869d48be45ceb
Shoji <stakashima21@gmail.com>
2020-01-02 23:22:08 -0800

    codewars(attempt): 11:22 PM

--- codewarsCodeChallenge/5-pig_latin.py
+++ codewarsCodeChallenge/5-pig_latin.py
@@ -10,3 +10,12 @@
 #   parse each word
 #   create a new string with the first letter last and add 'ay' 
 #   if it is a punctuation don't do anything
+
+def pig_it(text):
+    return ' '.join([word[1:] + word[:1] + 'ay' if word.isalpha() else word for word in text.split(' ')])
+    
+tests = ['hello', 'i am a pig', 'this has a punc!', 'why . am i ! a pig!']
+answers = ['ellohay', 'iay maay aay igpay', 'histay ashay aay uncpay!', 'hyway . maay iay ! aay igpay!']
+for test, answer in zip(tests, answers):
+    res = pig_it(test)
+    print(res, 'Success: ', res == answer)
