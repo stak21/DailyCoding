a216a1e4333561669d6315a8ff5a93fe58de2a0a
Shoji <stakashima21@gmail.com>
2020-01-02 23:19:37 -0800

    codewars(prep): 11:19 PM

--- codewarsCodeChallenge/5-pig_latin.py
+++ codewarsCodeChallenge/5-pig_latin.py
@@ -1,5 +1,12 @@
 # 1/2/20 11:13 PM
 
 # Requirements:
+#   Given a string, turn it into pig latin except for any punctuation
 #  I/O:
+#   hello => ellohay
+#   i am a pig => iay maay aay igpay
+#   This has a punc! => histay ashay aay uncpay!
 # Process:
+#   parse each word
+#   create a new string with the first letter last and add 'ay' 
+#   if it is a punctuation don't do anything
