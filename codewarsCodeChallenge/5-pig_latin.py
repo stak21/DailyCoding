2423ecddf912d6fa9051e6431ed2adcdf9eb9d08
Shoji <stakashima21@gmail.com>
2020-01-02 23:28:58 -0800

    codewars(success): 11:28 PM

--- codewarsCodeChallenge/5-pig_latin.py
+++ codewarsCodeChallenge/5-pig_latin.py
@@ -14,8 +14,10 @@
 def pig_it(text):
     return ' '.join([word[1:] + word[:1] + 'ay' if word.isalpha() else word for word in text.split(' ')])
     
-tests = ['hello', 'i am a pig', 'this has a punc!', 'why . am i ! a pig!']
-answers = ['ellohay', 'iay maay aay igpay', 'histay ashay aay uncpay!', 'hyway . maay iay ! aay igpay!']
+tests = ['hello', 'i am a pig', 'this has a punc !', 'why . am i ! a pig !']
+answers = ['ellohay', 'iay maay aay igpay', 'histay ashay aay uncpay !', 'hyway . maay iay ! aay igpay !']
 for test, answer in zip(tests, answers):
     res = pig_it(test)
     print(res, 'Success: ', res == answer)
+    if res != answer:
+        print("Expected", answer)
