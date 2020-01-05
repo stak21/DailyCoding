648c59897fd1a0587c6667c5ad05dcac57ebe3f1
Shoji <stakashima21@gmail.com>
2020-01-04 23:42:07 -0800

    codewars(r1 prep): 11:42 PM

--- codewarsCodeChallenge/reviews/r1-pig_latin.py
+++ codewarsCodeChallenge/reviews/r1-pig_latin.py
@@ -1,5 +1,11 @@
 # 1/6/20 11:37 PM
 
 # Requirements:
+#   translate a string into pig latin
 # I/O:
+#   hello => ellohay
+#   bye yo! => yebay oyay!
 # Process:
+#    parse the string 
+#   for each word create a new pig latin word
+# combine the parsed words and return
