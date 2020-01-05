001d2916988834bf9c8bf3ead990fe13b0b20a64
Shoji <stakashima21@gmail.com>
2020-01-04 23:46:10 -0800

    codewars(r1 test): 11:46 PM

--- codewarsCodeChallenge/reviews/r1-pig_latin.py
+++ codewarsCodeChallenge/reviews/r1-pig_latin.py
@@ -1,4 +1,5 @@
 # 1/6/20 11:37 PM
+# Test attempt 11:45 PM
 
 # Requirements:
 #   translate a string into pig latin
@@ -9,3 +10,14 @@
 #    parse the string 
 #   for each word create a new pig latin word
 # combine the parsed words and return
+
+def pigify(string):
+    return ' '.join([word[1:] + word[:1] + 'ay' if word.isalpha() else word for word in string.split(' ')])
+
+tests = ['hello', 'hello all', 'hello all !']
+answers = ['ellohay', 'ellohay llaay', 'ellohay llaay !']
+for test, answer in zip(tests, answers):
+    res = pigify(test)
+    print(res, 'Success:', res == answer)
+    if res != answer:
+        print('Expected: ', res)
