d474ea2f4ff8bdf1e823b5a6eb96919d14377c80
Shoji <stakashima21@gmail.com>
2020-01-05 23:22:21 -0800

    codewars(attemp1): 11:21PM

--- codewarsCodeChallenge/4-frequent_text.py
+++ codewarsCodeChallenge/4-frequent_text.py
@@ -1,5 +1,7 @@
 # 12/5/20 10:57 PM
 
+# 11:18 PM Attemp1
+
 # Requirements:
 #   Given a string of text, return the top three most frequent words
 #   matched words are case-insensitive
@@ -13,7 +15,38 @@
 # Process:
 #   Idea- Parse by spaces
 #           verify each item as a word
+#               How to veirfy? Easiest solution is to iterate through each letter and if it isn't alpha or an apostrephe next word
 #           if it is a word, append it to a dictionary with count of initial 1, or if it exists + 1
 #           else move on
 #           After that get the values and keys and sort them by the values
 #           return the first three
+
+def top_3_words(text):
+
+    def is_word(string):
+        for c in string:
+            if not c.isalpha() or not '\'':
+                return False
+        return True
+
+    parsed = text.split(' ')
+    cache = {}
+    for word in parsed:
+        word = word.lower()
+        if is_word(word):
+            if word in cache:
+                cache[word] +=1
+            else:
+                cache[word = 1]
+    counts = cache.items
+    counts.sort(lambda x: x[1])
+    return counts[:3]
+
+  tests = ['Hello there I', 'hello, I am hello be I want am hello', 'I can\'t hear you, but I can\'t hear']
+  answers = [['hello', 'there', 'i'], ['hello', 'i', 'am'], ['can\'t', 'i', 'hear'] ]
+
+for test, answer in zip(tests, answers):
+    res = top_3_words(test)
+    print(res, 'Success: ', res == answer)
+    if res != answer:
+        print("Expected", answer)
